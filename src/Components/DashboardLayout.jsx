3e536b8dc671dd76262a4b2d9fe61aee2ec5c1c2
Akshay-kumar-1234 <ap771316@gmail.com>
2026-03-01 00:18:26 +0530

    page errors 4 done

--- src/Components/DashboardLayout.jsx
+++ src/Components/DashboardLayout.jsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import "../Styles/dashboard.css";
 
-export default function DashboardLayout({ children, user }) {
+export default function DashboardLayout({ user, setUser, children }) {
   const navigate = useNavigate();
   const [open, setOpen] = useState(false);
 
